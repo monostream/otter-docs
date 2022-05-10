5f9ac4e1740f5f9fb1cfb4e2e2344da49bba39bd
Christian <christian@monostream.com>
2022-05-10 16:05:20 +0200

    include example in scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,3 +4,4 @@ set -euo pipefail
 cd ..
 
 helm upgrade --install otter-docs -n otter-docs -f indigo-values.yaml ./chart
+helm upgrade --install otter-docs-example -n otter-docs -f example/indigo-values.yaml ./example/chart
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
+az acr login -n tribblesregistry
+
 cd ..
 
 docker build --platform linux/amd64 . -t tribblesregistry.azurecr.io/otter-docs
 docker push tribblesregistry.azurecr.io/otter-docs
+
+cd example/
+
+docker build --platform linux/amd64 . -t tribblesregistry.azurecr.io/otter-docs-example
+docker push tribblesregistry.azurecr.io/otter-docs-example
