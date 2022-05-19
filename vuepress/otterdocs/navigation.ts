b3337c30d9e230bf9190b9f10cec741af08b2642
Christian <christian@monostream.com>
2022-05-19 16:10:13 +0200

    mehr otter navigations spass

--- vuepress/otterdocs/navigation.ts
+++ vuepress/otterdocs/navigation.ts
@@ -1,33 +1,46 @@
 import { readdirSync, statSync } from "fs"
-import { join } from "path"
+import { basename, extname, join } from "path"
 import { NavbarConfig } from "vuepress"
 
 const contentDir = 'docs'
 const contentPath = join(__dirname, '..', contentDir)
 
-const basePath = join('/', contentDir)
-
 export const getNavigation = () => {
-  const entries = readdirSync(contentPath)
-  const result: NavbarConfig = []
+  const entries: NavbarConfig = readdirSync(contentPath)
+    // Get navigation item for each directory entry
+    .map(entry => getNavItem(join('/', contentDir, entry)))
+    // Filter out undefined items
+    .filter(entry => !!entry)
+    // Sort in ascending ASCII order
+    .sort()
 
-  for (const entry of entries) {
-    const stat = statSync(join(contentPath, entry))
+  return entries
+}
 
-    if (stat.isDirectory()) {
-      if (entry.startsWith('.')) {
-        continue
-      }
+export const getNavItem = (link: string) => {
+  const extension = extname(link)
+  const name = basename(link, extension).toLowerCase()
 
-      result.push({
-        text: entry,
-        link: join('/', contentDir, entry, '/')
-      })
+  // Markdown file
+  if (extension === '.md') {
+    if (name === 'readme') {
+      return undefined
+    }
+
+    return link
+  }
+
+  // Directory
+  if (statSync(join(__dirname, '..', link)).isDirectory()) {
+    if (name.startsWith('.')) {
+      return undefined
+    }
+
+    return {
+      text: basename(link),
+      link: join(link, '/'),
     }
   }
 
-  // Sort in ascending ASCII order
-  result.sort()
-
-  return result
+  return undefined
 }
